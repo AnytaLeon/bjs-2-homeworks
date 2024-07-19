1e029d182b1d63d89edae3b33ffc8732cafe4f0c
Anna Petelina <petelina_an@mail.ru>
2024-07-19 12:30:41 +0400

    Изменения в файле 1.base-concepts/task.js, решены 2 задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - (4 * a * c);
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push((- b) / (2 * a));
+  } else {
+    arr.push(((-b + Math.sqrt(d)) / (2 * a)), ((-b - Math.sqrt(d)) / (2 * a))); 
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = (percent / 100) / 12;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  return Number((monthlyPayment * countMonths).toFixed(2));
 }
